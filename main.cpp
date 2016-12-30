71c891ee0f88d3e1dd748e5cb4c59ba5d1a5ebfb
dastasio <davide.stasio@gmail.com>
2016-12-30 01:10:23 +0100

    GLFW WINDOW
    starting a glfw window

--- main.cpp
+++ main.cpp
@@ -4,18 +4,30 @@ using namespace std;
 
 int main( int argc, char* argv[]) {
 	
+	int w = 1024, h = 768;
 		GLFWwindow* window;
 		if( !glfwInit()) exit( EXIT_FAILURE);
-		window = glfwCreateWindow( 1024, 768, "glfw", NULL, NULL);
+		window = glfwCreateWindow( w, h, "glfw", NULL, NULL);
 		if( !window) {
 			glfwTerminate();
 			exit( EXIT_FAILURE);
 		}
 	glfwMakeContextCurrent( window);
 
-	// getting version info
-	const GLubyte* p = glGetString( GL_VERSION);
-	cout << "OpenGL Version: " << p << endl;
+	while( !glfwWindowShouldClose( window)) {
+		glViewport( 0, 0, w, h);
+		glClearColor( 0.2, 0.4, 0.5, 1.0);
+		glClear( GL_COLOR_BUFFER_BIT);
+
+		// draw code
+
+		glfwSwapBuffers( window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow( window);
+	glfwTerminate();
+
 	return 0;
 }
 
