d0c55a276d74f54ad5cf317cb00c2b89058f479d
dastasio <davide.stasio@gmail.com>
2017-01-03 18:26:31 +0100

    Glew implementation

--- main.cpp
+++ main.cpp
@@ -16,7 +16,35 @@ int main( int argc, char* argv[]) {
 		}
 	glfwMakeContextCurrent( window);
 
-	glewInit();
+	// initializing glew
+	glewExperimental = GL_FALSE;
+	GLenum glewError = glewInit();
+	if (glewError != GLEW_OK) {
+		printf("glew init error\n%s\n", glewGetErrorString(glewError));
+	}
+
+	if (!GLEW_VERSION_2_1) {
+		printf("OpenGL 2.1 not supported!");
+		return false;
+	}
+
+	// getting opengl and glsl version strings
+	const GLubyte* p = glGetString(GL_VERSION);
+	cout << "Graphics Driver: " << p << endl;
+
+	const GLubyte* q = glGetString(GL_SHADING_LANGUAGE_VERSION);
+	cout << "GLSL Version: " << q << endl;
+
+
+	// checking for functionalities
+	if (GLEW_ARB_vertex_array_object) {
+		printf("genVertexArrays supported\n");
+	}
+	if (GLEW_APPLE_vertex_array_object) {
+		printf("genVertexArrayApple supported\n");
+	}
+
+
 	while( !glfwWindowShouldClose( window)) {
 		glViewport( 0, 0, w, h);
 		glClearColor( 0.2, 0.4, 0.5, 1.0);
